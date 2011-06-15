17eba26b03831c63da2911907cd6d854405599b6
Jesse Dubay <jesse@thefortytwo.net>
2011-06-15 11:12:04 -0700

    Bump version number to 1.1.0.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
         name='discogs-client',
-        version='1.0.0',
+        version='1.1.0',
         description='Official Python API client for Discogs',
         url='https://github.com/discogs/discogs-python-client',
         author='Discogs',

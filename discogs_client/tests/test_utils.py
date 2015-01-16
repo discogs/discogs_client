4ee0893781e6ca6fc3cdb7380ed69b6225fc8452
Bruno Cauet <brunocauet@gmail.com>
2015-01-16 16:12:32 +0100

    Fix utils.update_qs test

--- discogs_client/tests/test_utils.py
+++ discogs_client/tests/test_utils.py
@@ -3,13 +3,17 @@ from datetime import datetime
 from discogs_client.tests import DiscogsClientTestCase
 from discogs_client import utils
 
+
 class UtilsTestCase(DiscogsClientTestCase):
     def test_update_qs(self):
         """update_qs helper works as intended"""
         u = utils.update_qs
         self.assertEqual(u('http://example.com', {'foo': 'bar'}), 'http://example.com?foo=bar')
         self.assertEqual(u('http://example.com?foo=bar', {'foo': 'baz'}), 'http://example.com?foo=bar&foo=baz')
-        self.assertEqual(u('http://example.com?c=3&a=yep', {'a': 1, 'b': '1'}), 'http://example.com?c=3&a=yep&a=1&b=1')
+        # be careful for dict iteration order is not deterministic
+        result = u('http://example.com?c=3&a=yep', {'a': 1, 'b': '1'})
+        self.assertIn(result, ('http://example.com?c=3&a=yep&a=1&b=1',
+                               'http://example.com?c=3&a=yep&b=1&a=1'))
 
     def test_omit_none(self):
         o = utils.omit_none
@@ -31,6 +35,7 @@ class UtilsTestCase(DiscogsClientTestCase):
         p = utils.parse_timestamp
         self.assertEqual(p('2012-01-01T00:00:00'), datetime(2012, 1, 1, 0, 0, 0))
         self.assertEqual(p('2001-05-25T00:00:42'), datetime(2001, 5, 25, 0, 0, 42))
+
 
 def suite():
     suite = unittest.TestSuite()

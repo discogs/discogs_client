7c0c1e3dccdb2c598f00b07ef455ba281f2b5f8c
Rodney Keeling <rodneykeeling@gmail.com>
2015-05-29 09:37:30 -0700

    Update version number 2.1.0 to 2.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
         name='discogs-client',
-        version='2.1.0',
+        version='2.2.0',
         description='Official Python API client for Discogs',
         url='https://github.com/discogs/discogs_client',
         author='Discogs',

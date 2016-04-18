b3e956a85978a785b80d3c0ada3de7b64e801947
Rodney Keeling <rodneykeeling@gmail.com>
2016-04-18 15:44:55 -0700

    Update init to correct version

--- discogs_client/__init__.py
+++ discogs_client/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version_info__ = 2, 0, 2
-__version__ = '2.0.2'
+__version_info__ = 2, 2, 1
+__version__ = '2.2.1'
 
 from discogs_client.client import Client
 from discogs_client.models import Artist, Release, Master, Label, User, \

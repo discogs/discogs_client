c159195a43ceb3def5a9be2acce43bbad71a2e32
Jesse Dubay <jesse@jessedubay.com>
2020-06-01 13:55:58 -0700

    Deprecate discogs_client

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
         name='discogs-client',
-        version='2.2.2',
+        version='2.3.0',
         description='Official Python API client for Discogs',
         url='https://github.com/discogs/discogs_client',
         author='Discogs',
         author_email='api@discogsinc.com',
         test_suite='discogs_client.tests',
         classifiers=[
-            'Development Status :: 5 - Production/Stable',
+            'Development Status :: 7 - Inactive',
             'Environment :: Console',
             'License :: OSI Approved :: BSD License',
             'Natural Language :: English',
